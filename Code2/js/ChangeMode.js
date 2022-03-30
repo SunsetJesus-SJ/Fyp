ec713f23312cd6d900328e298835f8ff099eefed
TungLi12 <93913107+TungLi12@users.noreply.github.com>
2022-03-30 18:28:27 +0800

    Add files via upload

---
+++ Code2/js/ChangeMode.js
@@ -0,0 +1,3 @@
+﻿function ChangeMode() {
+    window.open("MainPage.php");
+}
---
+++ Code2/js/cs.js
@@ -0,0 +1,8 @@
+function Staff(){
+    window.open("staffAC.php");
+}
+
+function Customer(){
+    window.open("customerAC.php");
+}
+
